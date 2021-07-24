d9044179659515d35b68d63cfeccc99cdc00b1fa
Alex <alexdeassis7@gmail.com>
2021-07-24 18:00:54 -0300

    clase 3 DEmo Spring -JSF - Hibernate Arq Legacy

---
+++ JavaServerFaces/src/main/java/com/epidata/walkAndBuy/dao/impl/CustomerDaoImpl.java
@@ -0,0 +1,27 @@
+package com.epidata.walkAndBuy.dao.impl;
+
+import java.util.Date;
+import java.util.List;
+
+import com.epidata.walkAndBuy.dao.CustomerDao;
+import com.epidata.walkAndBuy.model.Customer;
+
+import org.springframework.orm.hibernate3.support.HibernateDaoSupport;
+
+public class CustomerDaoImpl extends HibernateDaoSupport implements CustomerDao {
+
+//	insert into customer
+	public void addCustomer(Customer customer) {
+
+		customer.setCreatedDate(new Date());
+		getHibernateTemplate().save(customer);
+
+	}
+
+//	select * from customer
+	public List<Customer> findAllCustomer() {
+
+		return getHibernateTemplate().find("from Customer");
+
+	}
+}
